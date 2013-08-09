12229a3c35385ac0b9169922a7151bbffd1ee91e
Ryan Willoughby <ryan.willoughby@adobe.com>
2013-08-09 11:39:54 -0700

    a wheel in the ditch and a wheel in the ground

--- js/index.js
+++ js/index.js
@@ -50,19 +50,30 @@ var app = {
     scan: function() {
         console.log('scanning');
         try {
-            window.plugins.barcodeScanner.scan(function(args) {
-                console.log("Scanner result: \n" +
-                    "text: " + args.text + "\n" +
-                    "format: " + args.format + "\n" +
-                    "cancelled: " + args.cancelled + "\n");
+            var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+            scanner.scan( function (result) { 
+
+                alert("We got a barcode\n" + 
+                "Result: " + result.text + "\n" + 
+                "Format: " + result.format + "\n" + 
+                "Cancelled: " + result.cancelled);  
+
+               console.log("Scanner result: \n" +
+                    "text: " + result.text + "\n" +
+                    "format: " + result.format + "\n" +
+                    "cancelled: " + result.cancelled + "\n");
+                document.getElementById("info").innerHTML = result.text;
+                console.log(result);
                 /*
                 if (args.format == "QR_CODE") {
                     window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
                 }
                 */
-                document.getElementById("info").innerHTML = args.text;
-                console.log(args);
-        });
+
+            }, function (error) { 
+                console.log("Scanning failed: ", error); 
+            } );
         } catch (ex) {
             console.log(ex.message);
         }
